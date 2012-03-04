1ba52f8e43dee2e14409b1fd7e240a2f830d4ef1
Vincent Beck <vincent.beck@enseeiht.fr>
2012-03-04 14:08:30 +0100

    Tests table Members

--- lib/member.rb
+++ lib/member.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class Member < ActiveRecord::Base
 
 	has_many :utilisations
@@ -7,9 +9,29 @@ class Member < ActiveRecord::Base
 	# Login
 	validates :login, :presence => true
 	validates :login, :uniqueness => true
-	validates :login, :format => { :with => /[a-z0-9_-]{2,}/i, :on => :create }
+	validates :login, :format => { :with => /^[a-z0-9_-]{2,}$/i, :on => :create }
 	
 	# Password
+	validates :password, :presence => true
 	validates :password, :confirmation => true
+	
+	validates :password_confirmation, :presence => true
+	
+	# Redefine password and password_confirmation because of sha1 encryption
+	def password=(password)
+		@password = Digest::SHA1.hexdigest(password);
+	end
+	
+	def password
+		@password
+	end
+	
+	def password_confirmation=(password)
+		@password_confirmation = Digest::SHA1.hexdigest(password);
+	end
+	
+	def password_confirmation
+		@password_confirmation
+	end
 
 end
---
+++ spec/member_spec.rb
@@ -0,0 +1,105 @@
+$: << File.join(File.dirname(__FILE__), '..')
+
+require 'authentification'
+require 'lib/member'
+
+describe Member do
+
+	describe "Missing informations" do
+	
+		it "An empty member should not be valid" do
+			subject.valid?.should be_false
+		end
+		
+		it "Should not be be valid with no login" do
+			subject.valid?
+			subject.errors.messages[:login].include?("can't be blank").should be_true
+		end
+		
+		it "Should not be be valid with no password" do
+			subject.valid?
+			subject.errors.messages[:password].include?("can't be blank").should be_true
+		end
+		
+		it "Should not be be valid with no password confirmation" do
+			subject.valid?
+			subject.errors.messages[:password_confirmation].include?("can't be blank").should be_true
+		end
+	
+	end
+	
+	describe "Wrong informations" do
+	
+		subject do
+			m = Member.new
+			m.login = "vin%100" 		#% is forbidden
+			m.password = "password"
+			m
+		end
+		
+		it "Should not be valid with wrong informations" do
+			subject.valid?.should be_false
+		end
+		
+		 it "Should not be valid with a wrong login" do
+		 	subject.valid?
+			subject.errors.messages[:login].include?("is invalid").should be_true
+		 end
+		 
+		 it "Should not be valid with a short login" do
+		 	subject.login = "l"
+		 	subject.valid?
+			subject.errors.messages[:login].include?("is invalid").should be_true
+		 end
+	
+	end
+	
+	describe "Check password" do
+	
+		subject do
+			m = Member.new
+			m.login = "vin100"
+			m
+		end
+		
+		it "Should call the encryption sha1" do
+			Digest::SHA1.should_receive(:hexdigest).with("password").and_return("5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8")
+			subject.password = "password"
+		end
+		
+		it "Should have the password hash registered" do
+			subject.password = "password"
+			subject.password.should == "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"
+		end
+		
+		it "Should not be valid with a wrong password confirmation" do
+			subject.password = "pw1"
+			subject.password_confirmation = "pw2"
+			subject.valid?.should be_false
+		end
+		
+		it "Should be valid with a right password confirmation" do
+			subject.password = "pw1"
+			subject.password_confirmation = "pw1"
+			subject.valid?.should be_true
+		end
+	
+	end
+	
+	describe "Member valid" do
+		
+		subject do
+			m = Member.new
+			m.login = "my__pseudo--"
+			m.password = "pw"
+			m.password_confirmation = "pw"
+			m
+		end
+		
+		it "Should be valid" do
+			subject.valid?.should be_true
+		end
+		
+	end
+
+end
