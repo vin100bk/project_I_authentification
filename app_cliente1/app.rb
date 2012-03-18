f4163297d633ed1c93dd624840780877ae2c2ea1
Vincent Beck <vincent.beck@enseeiht.fr>
2012-03-18 16:16:54 +0100

    Modification du token de l'appli

--- app_cliente1/app.rb
+++ app_cliente1/app.rb
@@ -3,8 +3,6 @@ $: << File.dirname(__FILE__)
 require 'sinatra'
 require 'digest/sha1'
 
-# Specify public dir
-set :public_folder, File.dirname(__FILE__) + '/www'
 # Specify the port application
 set :port, 9191
 
@@ -16,14 +14,16 @@ get '/' do
 end
 
 # Protected section
-get '/protected/?' do
-	if !session[:current_user_app_1].nil? || (!params['token'].nil? && !params['login'].nil? && params['token'] == Digest::SHA1.hexdigest('0d438478f41655f34356f7564006e0589f194610' + params['login']))
-		# User connected
-		session[:current_user_app_1] ||= params['login']
-		erb :protected
-	else
-		redirect 'http://localhost:9090/App_1/session/new?origin=/protected'
-	end
-	
+get '/protected/?' do	
+if !session[:current_user_App1].nil? || (!params['token'].nil? && !params['login'].nil? && params['token'] == Digest::SHA1.hexdigest('84cdfa71d92b23d866a7698bf7d796ed886a8826' + params['login']))
+  # User connected
+  session[:current_user_App1] ||= params['login']
+  # Here is your protected section, put your private code
+  erb :protected
+elsif !params['token'].nil? && !params['login'].nil?
+  # Access refused
+  'You cannot access to this section !' else
+  redirect 'http://localhost:9090/App1/session/new?origin=' + request.path
+end 
 end
 
